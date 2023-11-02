e2b295b778c082cf4d27297a41caecdda4d06234
Raul <125rperezcortez@frhsd.com>
2023-11-02 09:48:32 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//Print out Hello, World to the console 
+console.log("Hello, World")
+
+/*this should 
+be a multiple 
+line comment*/
+
+console.log("Raul Perez Cortez")
+
+// DATA TYPES
+// Numbers
+console.log(2023);
+// Strings 
+console.log("That is the current year");
+// Booleans
+console.log(true);
+console.log(false);
+// Null 
+
+// Undefined 
+
+// Objects 
+var mycar = {
+    color: "Blue",
+    make: "Meza",
+    model: "CX-T",
+    year: "2023",
+    
+}
+
+var yourcar = {
+    color: "Green",
+    make: "Tesla",
+    model: "Model 3",
+    year: "2023",
+    
+}
+
+console.log(mycar); 
+// Print My Mazda is Blue 
+console.log("My " + mycar.make + " is " + mycar.color);
+// Print My Tesla s Blue 
+console.log("Your " + yourcar.make + " is " + mycar.color )
+
+// Math 
+
+//Add
+// +
+console.log( "2 + 2 = " + 2 + 2);
+console.log( 2 + 2 +  " 2 + 2");
+
+var solution = 2 + 2;
+console.log("2 + 2 = " + solution);
+
+//Subtract
+//-
+var newSolution;
+newSolution = solution - 5;
+ console.log("solution: " + solution);
+
+//Multiply
+// *
